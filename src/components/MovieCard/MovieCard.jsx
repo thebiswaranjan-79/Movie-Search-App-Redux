8c9bd2ea098dc5aa959ca6797c4c305ffd3eb1dc
Biswaranjan <mbiswaranjan797@gmail.com>
2024-04-12 00:57:14 +0530

    Added the Card Components

---
+++ src/components/MovieCard/MovieCard.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import './MovieCard.css';
+
+function MovieCard({Title, Year, Type, Poster}) {
+  return (
+    <div className='movie-wrapper'>
+        <div className='movie-image'>
+            <img src= {Poster} />
+        </div>
+        <div className='movie-title'>
+            <span>{Title}</span>
+        </div>
+        <div className='movie-year'>
+            <span>Release in : {Year}</span>
+        </div>
+        <div className='movie-type'>
+            <span>Type : {Type}</span>
+        </div>
+
+    </div>
+  )
+}
+
+export default MovieCard
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,31 @@
 import React from 'react'
 import './Home.css';
+import MovieCard from '../components/MovieCard/MovieCard';
 
 
 const Home = () => {
+
+  const movie = {
+    "Title":"Harry Potter and the Deathly Hallows: Part 2",
+    "Year":"2011",
+    "imdbID":"tt1201607",
+    "Type":"movie",
+    "Poster":"https://m.media-amazon.com/images/M/MV5BMGVmMWNiMDktYjQ0Mi00MWIxLTk0N2UtN2ZlYTdkN2IzNDNlXkEyXkFqcGdeQXVyODE5NzE3OTE@._V1_SX300.jpg"
+  };
+
   return (
     <>
-       {/*  Navbar  */}
-       {/*  MovieList */}
-       {/*  Pagination Button */}
-       Home page
+       <div className='movie-card-wrapper'>
+            <MovieCard
+                {...movie} // Destructure Object
+            />
+            <MovieCard
+                {...movie} // Destructure Object
+            />
+            <MovieCard
+                {...movie} // Destructure Object
+            />
+       </div>
     </>
   )
 };
